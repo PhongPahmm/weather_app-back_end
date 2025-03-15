3c107f6975e1aef893d91b9756ddf702deb065e8
Phong Pahmm <phongphp03@gmail.com>
2025-03-15 12:54:11 +0700

    update

--- src/main/java/spring/weatherapp/service/WeatherService.java
+++ src/main/java/spring/weatherapp/service/WeatherService.java
@@ -5,12 +5,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import spring.weatherapp.exception.CityNotFound;
 import spring.weatherapp.model.WeatherResponseCurrent;
 import spring.weatherapp.model.WeatherResponseDaily;
 
 @Service
 public class WeatherService {
-    @Value("${openweather.api.key}")
+    @Value("${openweather.api.key2}")
     private String apiKey;
 
     @Value("${openweather.api.url}")
@@ -25,19 +26,19 @@ public class WeatherService {
             return restTemplate.getForObject(url, WeatherResponseCurrent.class);
         }catch (HttpClientErrorException e) {
             if(e.getStatusCode().value() == 404){
-                return null;
+                throw new CityNotFound();
             }
             throw e;
         }
     }
 
     public WeatherResponseDaily getWeatherResponseDaily(String city) {
-        String url = apiUrl + "forecast?q=" + city + "&units=metric&cnt=81&appid=" + apiKey;
+        String url = apiUrl + "forecast?q=" + city + "&units=metric&cnt=40&appid=" + apiKey;
         try{
             return restTemplate.getForObject(url, WeatherResponseDaily.class);
         }catch (HttpClientErrorException e) {
             if(e.getStatusCode().value() == 404){
-                return null;
+                throw new CityNotFound();
             }
             throw e;
         }
